4bf7e2d96fe44e65a4194bc1896b40c2324e673c
Cali B <caliyodigurl@gmail.com>
2020-09-27 20:01:36 -0400

    User has posts

--- src/socialmedia/main/User.java
+++ src/socialmedia/main/User.java
@@ -1,11 +1,12 @@
 package socialmedia.main;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class User {
     private UUID uuid;
     public String firstname;
-    public List<Post> posts;
+    public ArrayList<Post> posts = new ArrayList<Post>();
     public List<User> following;
 }
--- src/socialmedia/test/UserSpec.java
+++ src/socialmedia/test/UserSpec.java
@@ -1,15 +1,33 @@
 package socialmedia.test;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import socialmedia.main.Post;
 import socialmedia.main.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserSpec {
-    @Test
-    public void userHasFirstname(){
-        User user = new User();
-        user.firstname = "Alice";
+    User alice = new User();
+    Post alicePost = new Post();
 
-        assertEquals(user.firstname, "Alice");
+
+    @BeforeEach
+    public void setUp() {
+        alice.firstname = "Alice";
+        alice.posts.add(alicePost);
+    }
+
+    @Test
+    public void userHasFirstname() {
+        String expectedName = "Alice";
+        assertEquals(alice.firstname, expectedName);
+    }
+
+    @Test
+    public void userHasPosts(){
+        assertEquals(alice.posts.size(), 1);
     }
 }
